c1f905fea94945dc7d8a44a37def453cf5a2dbf1
minseo121 <122244556+minseo121@users.noreply.github.com>
2024-11-09 23:17:59 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 172 ms, Memory: 57740 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -5,14 +5,20 @@ n = int(data[0])
 m = int(data[1])
 
 dict = {}
+dict2 = {}
 
 for i in range(1,n+1):
-    name = input()
+    name = sys.stdin.readline().strip()
     dict[i] = name
-    dict[name] = i
+    dict2[name] = i
+
+result = []
 
 for i in range(m):
     b = sys.stdin.readline().strip()
     if b.isdigit(): 
-        b= int(b)
-    sys.stdout.write(str(dict[b]) + '\n')
+        result.append(dict[int(b)])
+    else:
+        result.append(str(dict2[b]))
+
+sys.stdout.write("\n".join(result) + "\n")
