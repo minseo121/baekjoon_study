8cdfdccc5109d756a139a47c8dd4e7eb6e397fad
minseo121 <122244556+minseo121@users.noreply.github.com>
2025-01-24 14:26:53 +0900

    [Silver IV] Title: 판화, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.py
+++ 백준/Silver/1730. 판화/판화.py
@@ -1,49 +1,51 @@
-N = int(input())
-order = input()
-board = [["."]*N for _ in range(N)]
+import sys
+input = sys.stdin.readline
 
-x = 0
-y = 0
-count = 2
-key = True
-for i in order :
-    if i == "D" or i == "U" :
-        if i == "U" and  x == 0 :
-            continue
-        elif i == "D" and x == N-1:
-            continue
-        for j in range(count) :                   
-            if board[x][y]=="-":
-                board[x][y]="+"
-            elif board[x][y]==".":
-                board[x][y]="|"
-            if i =="D" :
-                if j == 0 :
-                    x+=1
-            else:
-                if j == 0 :
-                    x-=1   
-                
-    else :
-        if i == "L" and  y == 0 :
-                continue
-        elif i == "R" and y == N-1:
-            continue
-        for j in range(count) :
- 
-            if board[x][y]=="|":
-                board[x][y]="+"
-            elif board[x][y]==".":
-                board[x][y]="-"
-            if i =="R" :
-                if j == 0 :
-                    y+=1
-            else:
-                if j == 0 :
-                    y-=1   
-                
-                
-for i in range(N):
-    for j in range(N):
-        print(f'{board[i][j]}',end="")
-    print()
+# 입력 받기
+n = int(input().strip())  # 데이터 크기
+data = [['.'] * n for _ in range(n)]  # 초기 빈 데이터
+message = input().strip()  # 이동 명령어
+x, y = 0, 0  # 시작 위치
+
+# 이동 처리
+for m in message:
+    if m == 'D':  # 아래로 이동
+        if x + 1 < n:  # 경계값 확인
+            for j in range(2):
+                if data[x][y] == '-':
+                    data[x][y] ='+'
+                elif data[x][y] == '.':
+                    data[x][y] = '|'
+                if j == 0:
+                    x += 1
+                    
+    elif m == 'U':  # 위로 이동
+        if x - 1 >= 0:  # 경계값 확인
+            for j in range(2):
+                if data[x][y] == '-':
+                    data[x][y] ='+'
+                elif data[x][y] == '.':
+                    data[x][y] = '|'
+                if j == 0:
+                    x -= 1
+    elif m == 'L':  # 왼쪽으로 이동
+        if y - 1 >= 0:  # 경계값 확인
+            for j in range(2):
+                if data[x][y] == '|':
+                    data[x][y] ='+'
+                elif data[x][y] == '.':
+                    data[x][y] = '-'
+                if j == 0:
+                    y -= 1
+    elif m == 'R':  # 오른쪽으로 이동
+        if y + 1 < n:  # 경계값 확인
+            for j in range(2):
+                if data[x][y] == '|':
+                    data[x][y] ='+'
+                elif data[x][y] == '.':
+                    data[x][y] = '-'
+                if j == 0:
+                    y += 1
+    
+for row in data:
+    print(''.join(row))
